8d05df0a6e09abc777f1005fda0b529cd0403882
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2022-03-01 21:05:11 +0100

    feat(app): use knex & update queries in consequence

---
+++ TD1/service/api/knex.js
@@ -0,0 +1,13 @@
+const knex = require ('knex')({
+    client : 'mysql',
+    version : '8.0.23',
+    connection: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+    }
+});
+
+module.exports = knex;
--- TD1/service/api/routes/commandes.js
+++ TD1/service/api/routes/commandes.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var router = express.Router()
 
-const db = require('../db')
+const db = require('../knex')
 
 const uuidv4 = require('uuid').v4
 
@@ -48,60 +48,62 @@ router.route('/')
       total += item.tarif * item.quantite
     })
 
-    db.query('INSERT INTO commande (id, nom, mail, livraison, montant, token, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [uuid, req.body.nom, req.body.mail, livraison, total, token, new Date(), new Date()], function (err, result) {
-      if (err) {
-        res.status(500).json({
-          type: "error",
-          error: 500,
-          message: "erreur lors de la creation de la commande"
-        })
-      } else {
-        items.forEach(item => {
-          db.query('INSERT INTO item (libelle, uri, quantite, tarif, command_id) VALUES (?, ?, ?, ?, ?)', [item.libelle, item.uri, item.quantite, item.tarif, uuid], function (err, results) {
-            if (err) {
-              res.status(500).json({
-                type: "error",
-                error: 500,
-                message: "erreur lors de la creation des items de la commande"
-              })
-            }
+    try {
+      db.transaction(trx => {
+        trx.insert({
+          id: uuid,
+          nom: req.body.nom,
+          mail: req.body.mail,
+          livraison: livraison,
+          montant: total,
+          token: token,
+          created_at: new Date(),
+          updated_at: new Date()
+        }).into('commande').then(async function (data) {
+          items.forEach(item => {
+            trx.insert({
+              libelle: item.libelle,
+              uri: item.uri,
+              quantite: item.quantite,
+              tarif: item.tarif,
+              commande_id: uuid
+            }).into('item')
           })
-        })
-
-        res.status(201).json({
-          "commande": {
-            "id": uuid,
-            "nom": req.body.nom,
-            "mail": req.body.mail,
-            "livraison": livraison,
-            "montant": total,
-            "token": token,
-          }
-        })
-      }
-    })
+          const commande = await trx.select('*').from('commande').where('id', uuid)
+          res.status(201).json(commande)
+        }).then(trx.commit)
+          .catch(trx.rollback)
+      })
+    } catch (error) {
+      return res.status(500).json({
+        type: "error",
+        error: 500,
+        message: "erreur lors de la creation de la commande"
+      })
+    }
   })
   .put(methodNotAllowed)
   // GET ALL COMMANDES
-  .get(function (req, res, next) {
-    db.query('SELECT * FROM commande', (err, results) => {
-      if (err)
-        res.status(500).json({
-          type: "error",
-          error: 500,
-          message: "une erreur est survenue :" + err.message
+  .get(async function (req, res, next) {
+    try {
+      const commandes = await db.select().from('commande')
+      if (commandes.length > 0) {
+        res.json(commandes)
+      }
+      else {
+        res.status(404).json({
+          "type": "error",
+          "error": 404,
+          "message": "ressource non disponible : /commandes/"
         })
-      else
-        if (results.length === 0) {
-          res.status(404).json({
-            type: "error",
-            error: 404,
-            message: "ressource non disponible : /commandes/"
-          })
-        } else {
-          res.json(results)
-        }
-    })
+      }
+    } catch (error) {
+      res.status(500).json({
+        type: "error",
+        error: 500,
+        message: "erreur lors de la recuperation des commandes"
+      })
+    }
   })
 
 router.route('/:id')
@@ -110,84 +112,74 @@ router.route('/:id')
   .patch(methodNotAllowed)
   .post(methodNotAllowed)
   // EDIT ONE COMMANDE
-  .put(function (req, res, next) {
+  .put(async function (req, res, next) {
     req.body.updated_at = new Date()
-    db.query('UPDATE commande SET ? WHERE id = ?', [req.body, req.params.id], (err, results) => {
-      if (err)
-        res.status(500).json({
-          type: "error",
-          error: 500,
-          message: "une erreur est survenue : " + err.message
-        })
-      else if (results.affectedRows === 0)
+    try {
+      const affectedRows = await db.select().from('commande').where('id', req.params.id).update(req.body)
+      const commande = (await db.select().from('commande').where('id', req.params.id))[0]
+      if (affectedRows)
+        res.json(commande)
+      else
         res.status(404).json({
           type: "error",
           error: 404,
           message: "la commande " + req.params.id + " n'a pas ete trouvee : "
         })
-      else
-        res.status(204).json(results)
-    })
+    }
+    catch (error) {
+      res.status(500).json({
+        type: "error",
+        error: 500,
+        message: "une erreur est survenue : " + error.message
+      })
+    }
   })
   // GET ONE COMMANDE
-  .get(function (req, res, next) {
-    // Bon on a pas les items dans la requete ça marche pas
-    let toReturn = {}
+  .get(async function (req, res, next) {
     if (req.query.token || req.headers.x_lbs_token) {
-      db.query('SELECT * FROM commande WHERE id = ? AND token = ?', [req.params.id, req.query.token || req.headers.x_lbs_token], (err, results) => {
-        if (err) {
-          res.status(500).json({
-            type: "error",
-            error: 500,
-            message: "une erreur est survenue : " + err.message
-          })
-        } else {
-          if (results.length === 0) {
-            res.status(404).json({
-              type: "error",
-              error: 404,
-              message: "ressource non disponible : /commandes/" + req.params.id
-            })
-          } else {
-            toReturn = results[0]
-            let items = []
-            if (req.query.embed === 'items') {
-              db.query('SELECT * FROM item WHERE command_id=?', [req.params.id], (error, resu) => {
-                if (error)
-                  res.status(500).json({
-                    type: "error",
-                    error: 500,
-                    message: "une erreur est survenue :" + error.message
-                  })
-                else {
-                  if (resu.length === 0) {
-                    res.status(404).json({
-                      type: "error",
-                      error: 404,
-                      message: "ressource non disponible : /commandes/" + req.params.id + "/items"
-                    })
-                  }
-                  else {
-                    items = resu
-                  }
-                }
+      const token = req.query.token || req.headers.x_lbs_token
+      try {
+        const commande = (await db.select().from('commande').where('id', req.params.id).andWhere('token', token))[0]
+        if (commande) {
+          if (req.query.embed === "items") {
+            try {
+              const items = await db.select().from('item').where('command_id', req.params.id)
+              if (items.length > 0)
+                commande.items = items
+            } catch (error) {
+              res.status(500).json({
+                type: "error",
+                error: 500,
+                message: "une erreur est survenue : " + error.message
               })
-              toReturn.items = items
             }
-
-            toReturn.links = {
-              self: {
-                href: "/commandes/" + toReturn.id
-              },
-              items: {
-                href: "/commandes/" + toReturn.id + "/items"
-              }
-            }
-
-            res.json(toReturn)
           }
+
+          commande.links = {
+            self: {
+              href: "/commandes/" + commande.id
+            },
+            items: {
+              href: "/commandes/" + commande.id + "/items"
+            }
+          }
+
+          res.json(commande)
         }
-      })
+        else
+          res.status(404).json({
+            type: "error",
+            error: 404,
+            message: "ressource non disponible : /commandes/" + req.params.id
+          })
+
+      } catch (error) {
+        res.status(500).json({
+          type: "error",
+          error: 500,
+          message: "une erreur est survenue : " + error.message
+        })
+      }
     } else
       res.status(403).json({
         type: "error",
@@ -203,26 +195,24 @@ router.route('/:id/items')
   .post(methodNotAllowed)
   .put(methodNotAllowed)
   // GET ALL ITEMS OF ONE COMMANDE
-  .get(function (req, res, next) {
-    db.query('SELECT * FROM item WHERE command_id=?', [req.params.id], (err, results) => {
-      if (err)
-        res.status(500).json({
+  .get(async function (req, res, next) {
+    try {
+      const items = await db.select().from('item').where('command_id', req.params.id)
+      if (items.length > 0)
+        res.json(items)
+      else
+        res.status(404).json({
           type: "error",
-          error: 500,
-          message: "une erreur est survenue :" + err.message
+          error: 404,
+          message: "ressource non disponible : /commandes/" + req.params.id + "/items"
         })
-      else {
-        if (results.length === 0) {
-          res.status(404).json({
-            type: "error",
-            error: 404,
-            message: "ressource non disponible : /commandes/" + req.params.id + "/items"
-          })
-        } else {
-          res.json(results)
-        }
-      }
-    })
+    } catch (error) {
+      res.status(500).json({
+        type: "error",
+        error: 500,
+        message: "une erreur est survenue : " + error.message
+      })
+    }
   })
 
 function methodNotAllowed(req, res, next) {
