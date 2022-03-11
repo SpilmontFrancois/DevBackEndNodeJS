b95f8bc0f4a903459e3abfabf9f9ebbf46701210
sakerlucas <sakerlucas@gmail.com>
2022-03-11 11:58:51 +0100

    Auth

--- Projet/service/api_auth/app.js
+++ Projet/service/api_auth/app.js
@@ -3,8 +3,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
 
 var app = express();
 
@@ -14,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/auth', authRouter);
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
