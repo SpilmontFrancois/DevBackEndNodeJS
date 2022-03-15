c74f92dc82d5edf0d2c30cec9b9528a024b60047
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2022-03-15 18:01:01 +0100

    feat(middleware): finish middleware

--- Projet/service/api_gateway_back_office/middlewares/authMiddleware.js
+++ Projet/service/api_gateway_back_office/middlewares/authMiddleware.js
@@ -2,16 +2,24 @@ const jwt = require('jsonwebtoken')
 
 module.exports = function (req, res, next) {
     try {
-        if (!req.path.includes('/auth')) {
+        if (!req.path.includes('/auth'))
             if (req.headers.authorization) {
                 const token = req.headers.authorization.split(' ')[1]
-                jwt.verify(token, process.env.JWT_SECRET)
+                if (jwt.verify(token, process.env.JWT_SECRET))
+                    next()
+                else
+                    throw new Error()
             }
-        }
-        next()
+            else
+                throw new Error()
+        else
+            next()
     } catch (error) {
-        //res.send(error)
-        console.log(error);
+        res.status(401).json({
+            "type": "error",
+            "error": 401,
+            "message": "Vous devez être connecté pour accéder à cette ressource"
+        })
     }
 
 }
