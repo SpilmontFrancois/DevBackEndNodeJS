cdf1f27b80499078509dd060d333deb635ff374c
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2022-03-15 17:48:25 +0100

    feat(app): add auth routes tou gateway backoffice

--- Projet/service/api_gateway_back_office/routes/authService.js
+++ Projet/service/api_gateway_back_office/routes/authService.js
@@ -6,13 +6,13 @@ const BASE_URL = 'http://api_auth:3003'
 const api = apiAdapter(BASE_URL)
 
 router.post('/signin', (req, res) => {
-  api.post('/auth/signin' + req.path).then(resp => {
+  api.post('/auth/signin', req.body).then(resp => {
     res.json(resp.data)
   })
 })
 
 router.post('/signup', (req, res) => {
-  api.post('/auth/signup' + req.path).then(resp => {
+  api.post('/auth/signup', req.body).then(resp => {
     res.json(resp.data)
   })
 })
